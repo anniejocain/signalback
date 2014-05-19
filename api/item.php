9751731b9daa6fb7adbc40698ec9c1bea6b0ee1a
Matt Phillips <mphillips@awesome.law.harvard.edu>
2014-05-19 14:43:50 -0400

    Reworked the image resizing/slicing a little.

--- api/item.php
+++ api/item.php
@@ -119,51 +119,30 @@ class Item extends Controller {
         ////////////
         // Resize the image
         ////////////
+	$percent = 0.5;
 
-	// We now have the resized image. Let's crop it.
-	$thumb_width = 640;
-	$thumb_height = 480; 
+	// Get new dimensions
+	list($width, $height) = getimagesize($image_disk_path);
+	$new_width = $width * $percent;
+	$new_height = $height * $percent;
 
-	$width = imagesx($image);
-	$height = imagesy($image);
-
-	$original_aspect = $width / $height;
-	$thumb_aspect = $thumb_width / $thumb_height;
-
-	if ( $original_aspect >= $thumb_aspect )
-	{
-   		// If image is wider than thumbnail (in aspect ratio sense)
-   		$new_height = $thumb_height;
-   		$new_width = $width / ($height / $thumb_height);
-	}
-	else
-	{
-   		// If the thumbnail is wider than the image
-   		$new_width = $thumb_width;
-   		$new_height = $height / ($width / $thumb_width);
-	}
-
-
-	$thumb = imagecreatetruecolor( $thumb_width, $thumb_height );
-
+	// Resample
+	$thumb = imagecreatetruecolor($new_width, $new_height);
 	$image = imagecreatefrompng($image_disk_path);
+	imagecopyresampled($thumb, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
-	imagecopyresampled($thumb,
-        	           $image,
-                	   #0, // Center the image horizontally
-	                   0 - ($new_width - $thumb_width) / 2, // Center the image horizontally
-	       	           #0 - ($new_height - $thumb_height) / 2, // Center the image vertically
-			   0,
-	                   0, 0,
-	                   $new_width, $new_height,
-	                   $width, $height);
-	imagepng($thumb, $image_disk_path, 8);
+
+	// Some captures are super long. Trim their height to 500px.
+	if ($new_height > 500) {
+    		$thumb_slice = imagecreatetruecolor($new_width, 500);
+    		imagecopy($thumb_slice, $thumb, 0, 0, 0, 0, $new_width, 500);
+	}
+
+	// Write it back out to disk
+	imagepng($thumb_slice, $image_disk_path, 8);
         ////////////
         // Done resizing the image
         ////////////
-
-
-
       
     }
     
