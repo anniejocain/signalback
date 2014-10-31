cbc72444773f445e74a171d48515792be8ec7824
Matt Phillips <mphillips@law.harvard.edu>
2014-10-31 13:54:44 -0400

    Added some super basic fixture data

--- roundup_web/roundup/urls.py
+++ roundup_web/roundup/urls.py
@@ -1,13 +1,20 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 
+from roundup.api import ItemResource
+
+item_resource = ItemResource()
 
 urlpatterns = patterns('roundup.views',
 
     # Common Pages
     url(r'^/?$', 'common.landing', name='common_landing'),
+
+
+    # Tastypie urls. We might want to move these into their own file
+    (r'^api/', include(item_resource.urls)),
 )
