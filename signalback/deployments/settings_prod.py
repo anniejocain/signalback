209a403dc187cdd9197384d539fe0cad443a5edf
Matt Phillips <matt@mattphillips.info>
2014-11-29 17:10:23 -0500

    Heroku lameness

--- signalback/deployments/settings_prod.py
+++ signalback/deployments/settings_prod.py
@@ -33,6 +33,8 @@ STATICFILES_DIRS = (
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_SECURE_URLS = False       # use http instead of https
 AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related query parameters for requests
-AWS_S3_ACCESS_KEY_ID = ''     # enter your access key id
-AWS_S3_SECRET_ACCESS_KEY = '' # enter your secret access key
-AWS_STORAGE_BUCKET_NAME = ''
+
+# you should set these as config var in heroku, or in a local_settings file
+# AWS_S3_ACCESS_KEY_ID = ''     # enter your access key id
+# AWS_S3_SECRET_ACCESS_KEY = '' # enter your secret access key
+# AWS_STORAGE_BUCKET_NAME = ''
