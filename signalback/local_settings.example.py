e6599da8ff2dfc40df64ad1b24f0363d5f50819d
Matt Phillips <matt@mattphillips.info>
2014-11-30 16:18:45 -0500

    Freshened up sample settings

--- signalback/local_settings.example.py
+++ signalback/local_settings.example.py
@@ -2,9 +2,26 @@
 # from .deployments.settings_dev import *
 # from .deployments.settings_prod import *
 
-# Make this unique, and don't share it with anybody.
+#### Stuff needed for the Heroku deploy - start ####
+# We have a fab command to tack this stuff onto settings.py for heroku
 SECRET_KEY = ''
 
-AWS_S3_ACCESS_KEY_ID = ''     # enter your access key id
-AWS_S3_SECRET_ACCESS_KEY = '' # enter your secret access key
+AWS_ACCESS_KEY_ID = ''     # enter your access key id
+AWS_SECRET_ACCESS_KEY = '' # enter your secret access key
 AWS_STORAGE_BUCKET_NAME = ''
+
+S3_URL = 'http://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
+
+# swap this to cleardb
+HEROKU_DATABASES = {
+    'default': {
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+#### Stuff needed for the Heroku deploy - end ####
