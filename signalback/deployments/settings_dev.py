f3123df0dc05d604969cd0db1f187a509fdd9c6f
Matt Phillips <matt@mattphillips.info>
2014-11-30 16:54:59 -0500

    A little local dev settings cleanup

--- signalback/deployments/settings_dev.py
+++ signalback/deployments/settings_dev.py
@@ -6,14 +6,26 @@ TEMPLATE_DEBUG = DEBUG
 # Print email to console. Handy in a dev env.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Our local dev DB
+# You probably want to set some of this in your local settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sb',                      # Or path to database file if using sqlite3.
+        'USER': 'sb',                      # Not used with sqlite3.
+        'PASSWORD': 'sb',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Additional locations of static files
-#STATICFILES_DIRS = (
-#    'static',
+STATICFILES_DIRS = (
+    'static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-#)
+)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
