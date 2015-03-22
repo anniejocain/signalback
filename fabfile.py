080660f366cb4aaed43720412c1dcd8ce4e0ef3b
Matt Phillips <matt@mattphillips.info>
2015-03-22 12:30:03 -0400

    Added env config for aws cli

--- fabfile.py
+++ fabfile.py
@@ -29,14 +29,22 @@ def push_to_heroku():
         myfile.write("\nEMAIL_HOST_PASSWORD='%s'" % EMAIL_HOST_PASSWORD)
         myfile.write("\nEMAIL_USE_TLS=%s" % EMAIL_USE_TLS)
         myfile.write("\nEMAIL_USE_SSL=%s" % EMAIL_USE_SSL)
-    
-    # Generate our static files
-    local('python manage.py collectstatic')
 
+
+    with open(".aws/credentials", "a") as myfile:
+        myfile.write("aws_access_key_id = %s" % AWS_ACCESS_KEY_ID)
+        myfile.write("\naws_secret_access_key = %s\n" % AWS_SECRET_ACCESS_KEY)
+        
     # Toss our temp settings in git and push to heroku
     local("git commit -a -m 'heroku build'")
     local("git push heroku %s:master --force" % heroku_branch_name)
     local("git checkout %s" % current_branch)
     local("git branch -D %s" % heroku_branch_name)
+
+    # Generate our static files
+    local('heroku config:set AWS_ACCESS_KEY_ID=%s"' % AWS_ACCESS_KEY_ID)
+    local('heroku config:set AWS_SECRET_ACCESS_KEY=%s"' % AWS_SECRET_ACCESS_KEY)
+    local('heroku config:set AWS_DEFAULT_REGION=us-west-2"')
+    local('heroku run "aws s3 sync collected-static/. s3://signalback"')
     
     print "Pushed temp branch, %s, to Heroku" % heroku_branch_name
